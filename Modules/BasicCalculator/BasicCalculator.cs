888c2fd8ede6abb6f0571393890d6f7c87482780
Fabian Hänchen <fabian.haenchen@quodata.de>
2019-11-22 14:25:47 +0100

    Schnittstelle nach außen

---
+++ Modules/BasicCalculator/BasicCalculator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Just4You.Modules.BasicCalculator
+{
+    public static class BasicCalculator
+    {
+        public static Double Evaluate(String Expression)
+        {
+            var Tokens = Lexer.Tokenize(Expression);
+            var Node = Parser.GetAst(Tokens);
+            return Node.Calculate().Value;
+        }
+    }
+}
