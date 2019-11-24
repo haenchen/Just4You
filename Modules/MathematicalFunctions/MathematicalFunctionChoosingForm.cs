38189d9b8417a3eefcf2a4999bcd021b33401ff3
Fabian Hänchen <haenchen.fabian@gmail.com>
2019-11-24 23:38:56 +0100

    Vergessen dass Math Nutzung erlaubt ist

--- Modules/MathematicalFunctions/MathematicalFunctionChoosingForm.cs
+++ Modules/MathematicalFunctions/MathematicalFunctionChoosingForm.cs
@@ -47,54 +47,14 @@ namespace Just4You.Modules.MathematicalFunctions
             var exponent = new Parameter("Exponent");
             if (ParamAborted(exponent))
                 return;
-            double result = Power(exponentiationBase.Value, exponent.Value);
+            double result = Math.Pow(exponentiationBase.Value, exponent.Value);
             output.Add(exponentiationBase.Input + " ^ " + exponent.Input + " = " + result.ToString().Replace(".", ","));
             this.Close();
         }
 
-        private double Power(double exponentiationBase, double power, double precision = 0.000001)
-        {
-            if (power < 0)
-                return 1 / this.Power(exponentiationBase, - power, precision);
-            if (power >= 10)
-                return Square(this.Power(exponentiationBase, power / 2, precision / 2));
-            if (power >= 1)
-                return exponentiationBase * this.Power(exponentiationBase, power - 1, precision);
-            if (precision >= 1)
-                return SquareRoot(exponentiationBase);
-            return SquareRoot(this.Power(exponentiationBase, power * 2, precision * 2));
-        }
-
-        private double Square(double x)
-        {
-            return x * x;
-        }
-
-        // Newton Approximation
-        private double SquareRoot(double x)
-        {
-            if (x < 0)
-            {
-                GlobalLogger.addError("Nur positive Zahlen können radiziert werden.");
-                return Double.NaN;
-            }
-            double precision = 0.000001;
-            double y = 1.0;
-            while (Absolute(x/y - y) > precision)
-            {
-                y = (y + x / y) / 2;
-            }
-            return y;
-        }
-
-        private double Absolute(double x)
-        {
-            return x < 0 ? x * (-1) : x;
-        }
-
         private double NthRoot(double x, int n)
         {
-            return Power(x, 1.0 / n);
+            return Math.Pow(x, 1.0 / n);
         }
 
         private void btnRoot_Click(object sender, EventArgs e)
