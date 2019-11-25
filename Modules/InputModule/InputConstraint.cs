c89731abbe471aafc67e90b2c3379395b67cac10
Fabian Hänchen <haenchen.fabian@gmail.com>
2019-11-25 20:51:40 +0100

    Constraints angelegt

---
+++ Modules/InputModule/InputConstraint.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Just4You.Modules.InputModule
+{
+    public interface InputConstraint
+    {
+        bool IsValid(double x);
+    }
+
+    public class PositiveConstraint : InputConstraint
+    { 
+        public bool IsValid(double x)
+        {
+            return x > 0;
+        }
+    }
+
+    public class NonZeroConstraint : InputConstraint
+    {
+        public bool IsValid(double x)
+        {
+            return x != 0;
+        }
+    }
+
+    public class IntegerConstraint : InputConstraint
+    {
+        public bool IsValid(double x)
+        {
+            return x % 1 == 0;
+        }
+    }
+}
